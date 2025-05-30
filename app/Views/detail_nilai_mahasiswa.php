fa5758cf84f50072928944517d0933e1fcb12e8e
johankevin2202@gmail.com <johankevin2202@gmail.com>
2025-05-30 13:18:12 +0700

    menghapus script di detail nilai form penilaian

--- app/Views/detail_nilai_mahasiswa.php
+++ app/Views/detail_nilai_mahasiswa.php
@@ -472,9 +472,6 @@
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap JS Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
   <?= $this->endSection(); ?>
